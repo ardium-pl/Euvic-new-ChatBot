67e03265f44512353d470e70b27395f0cf2e8a48
lolocob7 <lolocob7@gmail.com>
2024-11-19 19:51:04 +0100

    fix

--- src/meta-handling/whatsapp/whatsapp-client.ts
+++ src/meta-handling/whatsapp/whatsapp-client.ts
@@ -33,7 +33,7 @@ export class WhatsAppClient {
     };
 
     try {
-        logger.info("odpowiedź ai" + aiResponse); 
+      logger.info("odpowiedź ai" + JSON.stringify(aiResponse));
       const payload = createPayload(
         aiResponse.status === "success"
           ? aiResponse.formattedAnswer
