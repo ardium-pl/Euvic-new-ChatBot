014310f770d2a0afb58a9ec16a6386602c8f1670
Jakub Bródka <147922448+lolocob7@users.noreply.github.com>
2025-03-15 18:16:38 +0100

    Prod 1.0.3 (#17)
    * new endpoint
    
    * Update to new thread
    
    * language detection
    
    * some type updates and better language handling

--- src/sql-translator/sqlTranslatorRouter.ts
+++ src/sql-translator/sqlTranslatorRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { logger } from "../insert-data-to-db/utils/logger.js";
-import { executeSQL } from "./database/mySql.js";
-import { finalResponse, generateGPTAnswer, sqlResponse } from "./gpt/openAi.js";
-import { promptForAnswer, promptForSQL } from "./gpt/prompts.js";
 import { ChatHistoryHandler } from "../meta-handling/whatsapp/chat_history/getChatHistory.js";
+import { executeSQL } from "./database/mySql.js";
+import { finalResponse, generateGPTAnswer, languageResponse, sqlResponse } from "./gpt/openAi.js";
+import { promptForAnswer, promptForLanguageDetection, promptForSQL } from "./gpt/prompts.js";
+
 
 export const sqlTranslatorRouter = express.Router();
 
@@ -34,7 +35,7 @@ sqlTranslatorRouter.post("/language-to-sql", async (req, res) => {
         status: "success",
         question: userQuery,
         sqlStatement: "-",
-        formattedAnswer: "Rozpoczęto nowy wątek",
+        formattedAnswer: "Rozpoczęto nowy wątek/A new thread has been started",
         rawData: [],
       })
       return;
@@ -87,10 +88,18 @@ sqlTranslatorRouter.post("/language-to-sql", async (req, res) => {
       return;
     }
 
+    const language = await generateGPTAnswer(
+      promptForLanguageDetection(userQuery),
+      languageResponse,
+      "language"
+    );
+
+
+    logger.info("Language ", language?.language);
     // Log before formatting the result
     logger.info("🤖 Sending data to OpenAI for formatting...");
     const formattedAnswer = await generateGPTAnswer(
-      promptForAnswer(userQuery, sqlAnswer.sqlStatement, rows),
+      promptForAnswer(userQuery, sqlAnswer.sqlStatement, rows, language!.language),
       finalResponse,
       "final_response"
     );
